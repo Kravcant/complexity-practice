f0d67dd5d5564098e94010ac0d54f1fc3af872ba
anthonykravchishin <tonykravc53@gmail.com>
2025-10-16 14:46:49 -0700

    Added time complexity up to hasCommon

--- src/Main.java
+++ src/Main.java
@@ -173,15 +173,19 @@ public class Main {
   // not found" if appropriate
   // assume that each String is bounded by a constant length
   // What is the time complexity of your solution?
-  // YOUR ANSWER HERE
+  // O(1)
   public static String emailLookupEfficient(HashMap<String, String> namesToEmails, String queryName) {
-    return null;
+    if (namesToEmails.containsKey(queryName)) {
+      return namesToEmails.get(queryName);
+    } else {
+      return "Person not found";
+    }
   }
 
   // What is the time complexity of this method?
   // assume that each String is bounded by a constant length
   // (assume the set and list have the same number of elements)
-  // YOUR ANSWER HERE
+  // O(n) n = wordSet.length
   public static boolean hasCommon(HashSet<String> wordSet, ArrayList<String> wordList) {
     for (String word : wordSet) {
       if (wordList.contains(word)) {
